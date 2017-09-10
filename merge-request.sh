9dd06db9a6032020fd2b982201b0a12de17c69d4
Tobias L. Maier <me@tobiasmaier.info>
2017-09-10 11:15:22 +0200

    Rename PRIVATE_TOKEN to GITLAB_PRIVATE_TOKEN

--- merge-request.sh
+++ merge-request.sh
@@ -3,7 +3,7 @@
 [[ $HOST =~ ^https?://[^/]+ ]] && HOST="${BASH_REMATCH[0]}/api/v4/projects/"
 
 # Look which is the default branch
-TARGET_BRANCH=`curl --silent "${HOST}${CI_PROJECT_ID}" --header "PRIVATE-TOKEN:${PRIVATE_TOKEN}" | python3 -c "import sys, json; print(json.load(sys.stdin)['default_branch'])"`;
+TARGET_BRANCH=`curl --silent "${HOST}${CI_PROJECT_ID}" --header "PRIVATE-TOKEN:${GITLAB_PRIVATE_TOKEN}" | python3 -c "import sys, json; print(json.load(sys.stdin)['default_branch'])"`;
 
 # The description of our new MR, we want to remove the branch after the MR has
 # been closed
@@ -18,13 +18,13 @@ BODY="{
 
 # Require a list of all the merge request and take a look if there is already
 # one with the same source branch
-LISTMR=`curl --silent "${HOST}${CI_PROJECT_ID}/merge_requests?state=opened" --header "PRIVATE-TOKEN:${PRIVATE_TOKEN}"`;
+LISTMR=`curl --silent "${HOST}${CI_PROJECT_ID}/merge_requests?state=opened" --header "PRIVATE-TOKEN:${GITLAB_PRIVATE_TOKEN}"`;
 COUNTBRANCHES=`echo ${LISTMR} | grep -o "\"source_branch\":\"${CI_COMMIT_REF_NAME}\"" | wc -l`;
 
 # No MR found, let's create a new one
 if [ ${COUNTBRANCHES} -eq "0" ]; then
     curl -X POST "${HOST}${CI_PROJECT_ID}/merge_requests" \
-        --header "PRIVATE-TOKEN:${PRIVATE_TOKEN}" \
+        --header "PRIVATE-TOKEN:${GITLAB_PRIVATE_TOKEN}" \
         --header "Content-Type: application/json" \
         --data "${BODY}";
 
